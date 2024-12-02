038f6c0cc2d95e241ca1e70188b93f9406a2e410
Dushyant <bobsingh149@gmail.com>
2024-12-02 06:41:44 +0530

    add: Post Category

---
+++ lib/models/post_category.dart
@@ -0,0 +1,37 @@
+enum PostCategory {
+  currentlyReading,
+  barter,
+  favourite,
+}
+
+// Extension to get the display name for each category
+extension CategoryExtension on PostCategory {
+  String get displayName {
+    switch (this) {
+      case PostCategory.currentlyReading:
+        return 'Reading now';
+      case PostCategory.barter:
+        return 'Barter';
+      case PostCategory.favourite:
+        return 'Favourites';
+      default:
+        return '';
+    }
+  }
+}
+
+PostCategory? stringToPostCategory(String category) {
+  try {
+    return PostCategory.values
+        .firstWhere((e) => e.toString().split('.').last == category);
+  } catch (e) {
+    return null; // Handle the case where no match is found
+  }
+}
+
+final List<PostCategory> postCategoryList = [
+  PostCategory.currentlyReading,
+  PostCategory.barter,
+  PostCategory.favourite,
+];
+
